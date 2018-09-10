c6354b56f75f626f9e286c8c60db95264122d310
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-09-10 01:03:56 -0400

    ray background color

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 use std::io::{self, stdout};
 
+use ray::Ray;
+use vec::Vec3;
+
 mod ppm;
 mod ray;
 mod vec;
 
+fn color(ray: &Ray) -> Vec3 {
+    let unit_direction = ray.direction.unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.);
+    (1. - t) * Vec3(1., 1., 1.) + t * Vec3(0.5, 0.7, 1.)
+}
+
 fn main() -> Result<(), io::Error> {
-    let x = 200;
-    let y = 100;
-    let mut ppm = ppm::Writer::new(stdout(), x, y)?;
-    for j in (0..y).rev() {
-        for i in 0..x {
-            let r = i as f32 / x as f32;
-            let g = j as f32 / y as f32;
-            let b = 0.2;
-            let r = (255.99 * r) as u8;
-            let g = (255.99 * g) as u8;
-            let b = (255.99 * b) as u8;
+    let nx = 200;
+    let ny = 100;
+    let mut ppm = ppm::Writer::new(stdout(), nx, ny)?;
+
+    let lower_left_corner = Vec3(-2., -1., -1.);
+    let horizontal = Vec3(4., 0., 0.);
+    let vertical = Vec3(0., 2., 0.);
+    let origin = Vec3(0., 0., 0.);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+
+            let r = Ray {
+                origin,
+                direction: lower_left_corner + u * horizontal + v * vertical,
+            };
+            let c = color(&r);
+
+            let r = (255.99 * c.0) as u8;
+            let g = (255.99 * c.1) as u8;
+            let b = (255.99 * c.2) as u8;
+
             ppm.write_pixel(r, g, b)?;
         }
     }
